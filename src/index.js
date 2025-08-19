3c02fa7c5fcc3f5985b48781568c7f284f014041
Ethany0104 <ethy0104@gmail.com>
2025-08-19 20:14:25 +0900

    Version Update: v0.1.5

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // Tailwind CSS를 포함한 기본 CSS 파일
+import './index.css';
 import App from './App';
+// serviceWorkerRegistration 파일을 import 해야 합니다.
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,3 +12,12 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+// 이 부분의 주석을 풀거나, unregister()를 register()로 변경합니다.
+// serviceWorker.unregister(); -> serviceWorkerRegistration.register();
+serviceWorkerRegistration.register();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
